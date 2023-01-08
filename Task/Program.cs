33da9aa20d1f89c3660f0152496bb276b9cf7a08
gocman <andreyrbn@gmail.com>
2023-01-09 00:07:47 +0600

    add task

--- Task/Program.cs
+++ Task/Program.cs
@@ -22,13 +22,13 @@ string [] array = new string [] {"hello", "dog", "2", "Kazan", "cat", "world"};
 string[] GetResultArray(string[] array)
 {
     string[] resultArray = new string[array.Length];
-    int count = 0;
+    int k = 0;
     for (int i = 0; i < array.Length; i++)
     {
         if (array[i].Length <= 3)
         {
-            resultArray[count] = array[i];
-            count++;
+            resultArray[k] = array[i];
+            k++;
         }
 
     }
